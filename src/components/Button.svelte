fb4d3542655b87ca8791fe29d7880907d4f2aa7b
Sub-Zerro <jimi4kuryki@gmail.com>
2024-02-11 17:27:22 +0500

    sasdsdsa

---
+++ src/components/Button.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let cls
+    export let text
+    export let base = 0
+
+    function click() {
+        switch (text) {
+            case 'C':
+                base = 0
+                break
+            default:
+                base = text
+        }
+    }
+    
+    let f = cls + "text-white text-2xl bg-teal-900 rounded-full h-[10vh] w-[10vh] my-1 mx-1"
+</script>
+<button class={f} on:click={click}>{text}</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,41 @@
-<script lang="ts">
-    import '../app.css';
+<script>
+    import '../app.css'
+    import Button from '../components/Button.svelte';
     export let name = 'Lev';
+    let base = 0
 </script>
-<h3 class="text-teal-400">My name is {name}</h3>
+
+<div class="items-center justify-center flex">
+    <div class="">
+        <h1 class="text-white text-6xl pb-5 pr-5 justify-end flex font-bold">{base}</h1>
+        <div class="w-full flex justify-center">
+            <Button cls='active:bg-sky-500 bg-sky-900' text='C'/>
+            <Button cls='active:bg-sky-500 bg-sky-900' text='%'/>
+            <Button cls='active:bg-sky-500 bg-sky-900' text='⌫'/>
+            <Button cls='active:bg-sky-500 bg-sky-900' text='/'/></div>
+    
+            <div class="w-full flex justify-center">
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=7/>
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=8/>
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=9/>
+            <Button cls='active:bg-sky-500 bg-sky-900' text='X'/></div>
+    
+            <div class="w-full flex justify-center">
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=4/>
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=5/>
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=6/>
+            <Button cls='active:bg-sky-500 bg-sky-900' text='—'/></div>
+    
+            <div class="w-full flex justify-center">
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=1/>
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=2/>
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=3/>
+            <Button cls='active:bg-sky-500 bg-sky-900' text='+'/></div>
+    
+            <div class="w-full flex justify-center">
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=0/>
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=00/>
+            <Button cls='bg-emerald-700 active:bg-emerald-500' text=','/>
+            <Button cls='active:bg-sky-500 bg-sky-900' text='='/></div>
+    </div>
+</div>
