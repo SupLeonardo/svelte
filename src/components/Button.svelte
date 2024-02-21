717b1ee51dbca68a498617a4dc41031748509146
Sub-Zerro <jimi4kuryki@gmail.com>
2024-02-21 23:14:24 +0500

    dark mode!!!

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,12 +1,12 @@
 <script>
     export let logo
 </script>
-<div class=" border-stone-200 rounded-xl w-[32%] flex justify-center mt-8 shadow-sm p-2 h-[45%] border-2 hover:bg-stone-300 ">
+<div class=" border-stone-200 rounded-xl w-[32%] flex justify-center items-center mt-8 shadow-sm p-2 h-[45%] border-2 hover:bg-stone-300 ">
     {#if logo === 'apple'}
-        <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/mac-os.png" alt="mac-os"/>
+        <img class=" sm:h-[4vw] md:h-[6vw] lg:h-[3vw] h-[10vw] " src="https://img.icons8.com/ios-filled/50/mac-os.png" alt="mac-os"/>
     {:else if logo === 'google'}
-        <img width="48" height="48" src="https://img.icons8.com/fluency/48/google-logo.png" alt="google-logo"/>
+        <img class=" sm:h-[4vw] md:h-[6vw] lg:h-[3vw] h-[10vw]" src="https://img.icons8.com/fluency/48/google-logo.png" alt="google-logo"/>
     {:else if logo === 'microsoft'}
-        <img width="48" height="48" src="https://img.icons8.com/color/48/microsoft.png" alt="microsoft"/>
+        <img class=" sm:h-[4vw] md:h-[6vw] lg:h-[3vw] h-[10vw]" src="https://img.icons8.com/color/48/microsoft.png" alt="microsoft"/>
     {/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,32 @@
 <script>
     import '../app.css'
     import Button from '../components/Button.svelte';
-    export let name = 'Lev';
-    let base = 0
-    
-</script>
+
+    export let page = document.documentElement;
+    export const switcher = () => {
+        if (!page.classList.contains('dark')) {
+            page.classList.add('dark')
+        }
+        else if (page.classList.contains('dark')) {
+            page.classList.remove('dark')
+        }
+    }
+    </script>
 
 <div class=" flex flex-col h-[85vh] w-[80vw] lg:w-[50vw] sm:w-[65vw] bg-white rounded-xl shadow-xl columns-1 items-center px-[10%] ">
+    <div class="theme-switcher"></div>
     <img class=' h-[7vh] w-[7vh] mt-[2vh] ' src="https://img.icons8.com/ios/50/verified-account.png" alt="verified-account"/> <br />
-    <h1 class=" flex justify-center font-bold w-[45vw] xl:w-[20vw] text-4xl ">Welcome back</h1>
-    <h2 class=" mt-[1vh] text-2xl ">Please enter your details to sign in</h2>
+    <h1 class=" flex justify-center font-bold w-[45vw] xl:w-[20vw] text-xl ">Welcome back</h1>
+    <h2 class=" mt-[1vh] text-xl flex text-center ">Please enter your details to sign in</h2>
     <div class=" w-full h-[20%] flex justify-between">
         <Button logo='apple' />
         <Button logo='google' />
         <Button logo='microsoft' />
     </div>
     
-    <div class="bg-[#ebebeb] w-full h-1 flex justify-center ">
-        <div class=" mt-[-2%] w-[10%] bg-white flex justify-center">
-            <h1 class=' mt-[-5px] text-lg'>OR</h1>
+    <div class="bg-[#ebebeb]  w-full hidden h-1 md:flex justify-center ">
+        <div class=" mt-[-2%] w-[10%] bg-white  justify-center lg:flex hidden">
+            <h1 class=' mt-[-0.2vw] text-2xl '>OR</h1>
         </div>
     </div>
     
@@ -28,18 +36,18 @@
 
     <div class=" w-full items-center pt-3 flex justify-between">
         <div class="flex w-[50%] items-center">
-            <input type='checkbox' class=' h-6 w-6  focus:ring-0 ' />
-            <h1 class="ml-[3%] text-xl font-bold">Remember me</h1>
+            <input type='checkbox' class=' md:h-6 md:w-6 w-6 h-4 cursor-pointer focus:ring-0 ' />
+            <h1 class="ml-[3%] text-sm md:text-xl font-bold">Remember me</h1>
         </div>
-        <p class=" underline text-xl font-semibold">Forgot password?</p>
+        <p class=" underline text-sm md:text-xl  md:ml-[5vh] font-semibold cursor-pointer hover:text-[#000000c4]">Forgot password?</p>
     </div>
 
-    <div class=" bg-[#242527] w-full h-[6%] text-white text-xl flex justify-center items-center rounded-xl mt-[4%]">Sign in</div>
+    <button on:click={switcher} class=" bg-[#242527] w-full h-[6%] text-white text-xl flex hover:bg-[#000000c4] justify-center items-center rounded-xl my-[4vh]">Sign in</button>
 
     <div class="text-black flex text-xl w-full mt-[5%] justify-center">
-        <h1>Don't have an account yet?</h1>
+        <h1 class="">Don't have an account yet?</h1><br>
         <h1 class='text-white'>d</h1>
-        <h1 class='underline font-semibold'>Sign up</h1>
+        <h1 class='underline font-semibold cursor-pointer hover:text-[#000000c4]'>Sign up</h1>
     </div>
     
 </div>
