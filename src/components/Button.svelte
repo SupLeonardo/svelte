cd837d4bdcc035471d7c7aea723730f7447ebb08
Sub-Zerro <jimi4kuryki@gmail.com>
2024-02-24 14:35:21 +0500

    dark mode

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,7 +1,7 @@
 <script>
     export let logo
 </script>
-<div class=" border-stone-200 rounded-xl w-[32%] flex justify-center items-center mt-8 shadow-sm p-2 h-[45%] border-2 hover:bg-stone-300 ">
+<div class=" border-stone-200 rounded-xl w-[32%] flex dark:hover:bg-[#686868] dark:border-[#686868] justify-center items-center mt-8 shadow-sm p-2 h-[45%] border-2 hover:bg-stone-300 ">
     {#if logo === 'apple'}
         <img class=" sm:h-[4vw] md:h-[6vw] lg:h-[3vw] h-[10vw] " src="https://img.icons8.com/ios-filled/50/mac-os.png" alt="mac-os"/>
     {:else if logo === 'google'}
